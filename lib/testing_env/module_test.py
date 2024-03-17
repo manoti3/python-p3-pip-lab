57f5e00e082c90083a8ed26148c533d8808a1190
manoti3 <hillarymanoti0@gmail.com>
2024-03-17 20:24:56 +0300

    completed assignment

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -7,11 +7,11 @@ from versions import (
 def test_python_version():
     version_info = python_version()
     assert version_info.major == 3
-    assert version_info.minor == 8
+    assert version_info.minor == 10
 
 
 def test_requests_version():
-    assert requests_version() == "2.27.1"
+    assert requests_version() == "2.25.1"
 
 
 def test_pytest_version():
